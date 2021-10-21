f285a311600893659265257dd7002b8b3cb03634
Desislava Todorova <desi_14_94@abv.bg>
2021-10-21 11:16:11 +0300

    format files

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -28,18 +28,19 @@ const getAllProducts = async (req, res) => {
       "<=": "$lte",
     };
     const regEx = /\b(<|>|<=|>=|=)\b/g;
-    let filters = numericFilters.replace(regEx, (match) => 
-      `-${operatorMap[match]}-`
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
     );
-    const options = ['price', 'rating'];
-    filters = filters.split(',').forEach((item) => {
-      const [field, operator, value] = item.split('-');
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
       if (options.includes(field)) {
-        queryObject[field] = {[operator]: Number(value)};
+        queryObject[field] = { [operator]: Number(value) };
       }
     });
   }
-console.log(queryObject)
+  console.log(queryObject);
   let result = Product.find(queryObject);
   //sort
   if (sort) {
--- starter/models/product.js
+++ starter/models/product.js
@@ -1,33 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String, 
-        required:[true, 'Product name must be provided'],
+  name: {
+    type: String,
+    required: [true, "Product name must be provided"],
+  },
+  price: {
+    type: Number,
+    required: [true, "The price must be provided"],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: "{VALUE} is not supported",
     },
-    price:{
-        type:Number,
-        required:[true, 'The price must be provided']
-    },
-    featured:{
-        type: Boolean,
-        default: false
-    },
-    rating:{
-type: Number,
-default: 4.5,
-    },
-    createdAt:{
-        type: Date,
-        default:Date.now(),
-    },
-    company:{
-        type: String,
-        enum:{
-            values:['ikea', 'liddy', 'caressa', 'marcos'],
-            message: '{VALUE} is not supported'
-        },
-        
-    },
-})
-module.exports = mongoose.model('Product', productSchema);
+  },
+});
+module.exports = mongoose.model("Product", productSchema);
